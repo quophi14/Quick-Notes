8f8894ff239efd1493f48cb01099e30c082e8d9f
quophi14 <johnnywycliffe923@gmail.com>
2025-06-05 04:31:24 +0000

    added a landing page and initial sign up

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8QMm1jNXk_fIhHvzzq3KjsmAZ_waztVM",
+  authDomain: "quick-notes-3684a.firebaseapp.com",
+  projectId: "quick-notes-3684a",
+  storageBucket: "quick-notes-3684a.firebasestorage.app",
+  messagingSenderId: "135173587986",
+  appId: "1:135173587986:web:1055f301007e02324d44cb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
