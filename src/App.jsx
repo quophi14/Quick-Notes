1557307f01b811636cfd9dc52491b5c7679458ca
quophi14 <johnnywycliffe923@gmail.com>
2025-06-05 05:15:11 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import Landing from "./routes/Landing";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    
-      
-      <Routes>
-        <Route path="/" element = {<Landing/>}/>
-        <Route path="/login" element = {<Login/>}/>
-        <Route path="/signup" element = {<Signup/>}/>
-      </Routes>
-
-  
+    <>
+      <div className="min-h-screen bg-slate-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Notebook, LogOut, User } from "lucide-react";
+
+function Navbar() {
+  const navbar = useNavigate();
+
+  return (
+    <nav className="bg-white shadow-sm">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <Link to="/" className="flex items-center space-x-2">
+            <Notebook className="h-8 w-8 text-indigo-600" />
+            <span className="text-xl font-semibold text-gray-900">
+              QuickNotes
+            </span>
+          </Link>
+
+          <div className="flex items-center space-x-4">
+            <div className="space-x-4">
+              <Link to="/login"
+              className="text-sm font-medium text-gray-600 text-indigo-600 transition-colors">Login</Link>
+            </div>
+            <div>
+              <Link
+                to="/signup"
+                className="text-sm font-medium text-white px-4 py-2 bg-indigo-600 rounded-md hover:bg-indigo-700 transition-colors "
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
